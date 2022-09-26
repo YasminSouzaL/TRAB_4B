9e21714a63db0ed91c7d968feb8dbd0dae8a1683
Yasmin Souza <souzasmimi@gmail.com>
2022-09-26 20:43:05 -0300

    ok

---
+++ PHP/Novas_pags/terminal_ouro.php
@@ -0,0 +1,7 @@
+<?php 
+    session_start();
+    include "../cabecalho.php";
+?>
+<?php
+    echo"Oi";
+?>
--- PHP/perfil.php
+++ PHP/perfil.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
     include "cabecalho.php";
-	
 ?>
 
 <?php 
@@ -72,7 +71,7 @@
 
 	<div>
 	<p class="text-center">
-    <a class="btn btn-outline-primary col-4 border-info" id="loginButton" href="../PHP/login.php"> Terminal Ouro Verde </a>
+    <a class="btn btn-outline-primary col-4 border-info" id="loginButton" href="Novas_pags/terminal_ouro.php"> Terminal Ouro Verde </a>
      </p>
 	 <p class="text-center">
     <a class="btn btn-outline-primary col-4 border-info" id="loginButton" href="../PHP/login.php">Terminal Central</a>
